e1ff8f14f397e8645fd047d1a28d21fe2596417c
mortyobnoxious <42044258+mortyobnoxious@users.noreply.github.com>
2022-12-30 02:39:41 +0300

    Update eksitime.js

--- eksitime.js
+++ eksitime.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         EkşiTime
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/mortyobnoxious/EksiTime
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  show "timesince" on eksisozluk
+// @author       Morty
 // @match        https://eksisozluk.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=eksisozluk.com
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
